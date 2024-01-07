15b2b28f2489e8e5a6d549fd1f5df9625c5aef3c
Ashwin Chandrasekar <ashw.chandr@gmail.com>
2024-01-06 20:46:41 -0500

    Add hram and wram sections to address bus

--- include/address_bus.h
+++ include/address_bus.h
@@ -4,11 +4,17 @@
 
 #include <cstdint>
 #include <memory>
+#include <vector>
 
 
 const uint16_t kRomBank00Start = 0x0000;
 const uint16_t kRomBank00End = 0x3FFF;
 
+const uint16_t kWramStart = 0xC000;
+const uint16_t kWramEnd = 0xDFFF; // TODO adjust this for CGB switchable bank
+
+const uint16_t kHramStart = 0xFF80;
+const uint16_t kHramEnd = 0xFFFE;
 
 class AddressBus : public Addressable {
 public:
@@ -19,4 +25,6 @@ public:
         void Write(const uint16_t i, const uint8_t byte);
 
         std::shared_ptr<Addressable> cart_;
+        std::vector<uint8_t> wram_ = std::vector<uint8_t>(0x2000);
+        std::vector<uint8_t> hram_ = std::vector<uint8_t>(0x80);
 };
--- src/address_bus.cpp
+++ src/address_bus.cpp
@@ -10,6 +10,10 @@ AddressBus::~AddressBus() {}
 const uint8_t AddressBus::Read(const uint16_t addr) {
     if (addr >= kRomBank00Start && addr <= kRomBank00End) {
         return cart_->Read(addr);
+    } else if (addr >= kWramStart && addr <= kWramEnd) {
+        return wram_[addr - kWramStart];
+    } else if (addr >= kHramStart && addr <= kHramEnd) {
+        return hram_[addr - kHramStart];
     }
 
     std::cout << "Unimplemented memory read: " << std::hex << addr << "\n";
@@ -18,8 +22,12 @@ const uint8_t AddressBus::Read(const uint16_t addr) {
 
 void AddressBus::Write(const uint16_t addr, const uint8_t value) {
     if (addr >= kRomBank00Start && addr <= kRomBank00End) {
-        return cart_->Write(addr, value);
+        cart_->Write(addr, value);
+    } else if (addr >= kWramStart && addr <= kWramEnd) {
+        wram_[addr - kWramStart] = value;
+    } else if (addr >= kHramStart && addr <= kHramEnd) {
+        hram_[addr - kHramStart] = value;
+    } else {
+        std::cout << "Unimplemented memory write: " << std::hex << addr << "\n";
     }
-
-    std::cout << "Unimplemented memory write: " << std::hex << addr << "\n";
 }
