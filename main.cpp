cd425df45dc481a94d6d0e5ca15e424b445f4271
Ashwin Chandrasekar <ashw.chandr@gmail.com>
2024-01-07 22:06:37 -0500

    Print serial output to validate with Blargg test roms (all but interrupts passing)

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@
 #include "address_bus.h"
 #include "cart.h"
 #include "cpu.h"
+#include "io.h"
+#include "ppu.h"
+#include "timer.h"
+
+
+std::string message = "";
 
 
 std::vector<uint8_t> loadRom(const std::string& filename)
@@ -37,6 +43,15 @@ std::vector<uint8_t> loadRom(const std::string& filename)
 }
 
 
+void updateSerialDebugMessage(std::shared_ptr<IO> io) {
+    if (io->serialControl_ == 0x81) {
+        message += io->serialData_;
+
+        io->serialControl_ = 0x00;
+    }
+}
+
+
 int main(int argc, char** argv)
 {
     spdlog::set_level(spdlog::level::trace);
@@ -53,17 +68,37 @@ int main(int argc, char** argv)
 
     std::shared_ptr<AddressBus> addressBus = std::make_shared<AddressBus>();
     std::shared_ptr<CPU> cpu = std::make_shared<CPU>();
+    std::shared_ptr<Timer> timer = std::make_shared<Timer>();
+    std::shared_ptr<IO> io = std::make_shared<IO>();
+    std::shared_ptr<PPU> ppu = std::make_shared<PPU>();
 
     // connect up components
     addressBus->cart_ = cart;
+    addressBus->io_ = io;
+    addressBus->ppu_ = ppu;
     cpu->memory_ = addressBus;
+    io->timer_ = timer;
+    io->cpu_ = cpu;
+    io->ppu_ = ppu;
 
-    long long totalCycles = 0;
+    uint32_t lastCycles_ = 0;
     while (true) {
-        int cycles = cpu->Step();
-        if (cycles <= 0) {
+        if (cpu->Step() < 0) {
             std::cout << "Error in CPU step\n";
             break;
+        }
+
+        int elapsed = cpu->cycles_ - lastCycles_;
+        // timer ticks 4 times every m cycle
+        for (int i = 0; i < 4 * elapsed; i++) {
+            timer->Tick();
+            ppu->Tick();
+        }
+        lastCycles_ = cpu->cycles_;
+
+        updateSerialDebugMessage(io);
+        if (message.length() > 0) {
+            std::cout << "DEBUG: " << message << "\n";
         }
     }
 
