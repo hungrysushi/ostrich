3d233a38a227dd9f60786a4d23a30f636ce0f942
Ashwin Chandrasekar <ashw.chandr@gmail.com>
2024-01-07 22:04:48 -0500

    Update address bus to read and write to IO, VRAM, and OAM

--- include/address_bus.h
+++ include/address_bus.h
@@ -7,24 +7,39 @@
 #include <vector>
 
 
-const uint16_t kRomBank00Start = 0x0000;
-const uint16_t kRomBank00End = 0x3FFF;
+const uint16_t kRomBankStart = 0x0000;
+const uint16_t kRomBankEnd = 0x7FFF;
+
+const uint16_t kVramStart = 0x8000;
+const uint16_t kVramEnd = 0x9FFF;
 
 const uint16_t kWramStart = 0xC000;
 const uint16_t kWramEnd = 0xDFFF; // TODO adjust this for CGB switchable bank
 
+const uint16_t kOamStart = 0xFE00;
+const uint16_t kOamEnd = 0xFE9F;
+
+const uint16_t kIoStart = 0xFF00;
+const uint16_t kIoEnd = 0xFF7F;
+
 const uint16_t kHramStart = 0xFF80;
 const uint16_t kHramEnd = 0xFFFE;
+
+const uint16_t kInterruptFlags = 0xFF0F;
+const uint16_t kInterruptEnable = 0xFFFF;
+
 
 class AddressBus : public Addressable {
 public:
         AddressBus();
         virtual ~AddressBus();
 
-        const uint8_t Read(const uint16_t i);
-        void Write(const uint16_t i, const uint8_t byte);
+        const uint8_t Read(const uint16_t addr);
+        void Write(const uint16_t addr, const uint8_t value);
 
         std::shared_ptr<Addressable> cart_;
+        std::shared_ptr<Addressable> io_;
+        std::shared_ptr<Addressable> ppu_;
         std::vector<uint8_t> wram_ = std::vector<uint8_t>(0x2000);
         std::vector<uint8_t> hram_ = std::vector<uint8_t>(0x80);
 };
--- src/address_bus.cpp
+++ src/address_bus.cpp
@@ -8,12 +8,22 @@ AddressBus::AddressBus() {}
 AddressBus::~AddressBus() {}
 
 const uint8_t AddressBus::Read(const uint16_t addr) {
-    if (addr >= kRomBank00Start && addr <= kRomBank00End) {
+    if (addr >= kRomBankStart && addr <= kRomBankEnd) {
         return cart_->Read(addr);
+    } else if (addr >= kVramStart && addr <= kVramEnd) {
+        return ppu_->Read(addr);
     } else if (addr >= kWramStart && addr <= kWramEnd) {
         return wram_[addr - kWramStart];
+    } else if (addr >= kOamStart && addr <= kOamEnd) {
+        return ppu_->Read(addr);
+    } else if (addr >= kIoStart && addr <= kIoEnd) {
+        return io_->Read(addr);
     } else if (addr >= kHramStart && addr <= kHramEnd) {
         return hram_[addr - kHramStart];
+    } else if (addr == kInterruptFlags) {
+        return io_->Read(addr);
+    } else if (addr == kInterruptEnable) {
+        return io_->Read(addr);
     }
 
     std::cout << "Unimplemented memory read: " << std::hex << addr << "\n";
@@ -21,12 +31,22 @@ const uint8_t AddressBus::Read(const uint16_t addr) {
 }
 
 void AddressBus::Write(const uint16_t addr, const uint8_t value) {
-    if (addr >= kRomBank00Start && addr <= kRomBank00End) {
+    if (addr >= kRomBankStart && addr <= kRomBankEnd) {
         cart_->Write(addr, value);
+    } else if (addr >= kVramStart && addr <= kVramEnd) {
+        ppu_->Write(addr, value);
     } else if (addr >= kWramStart && addr <= kWramEnd) {
         wram_[addr - kWramStart] = value;
+    } else if (addr >= kOamStart && addr <= kOamEnd) {
+        ppu_->Write(addr, value);
+    } else if (addr >= kIoStart && addr <= kIoEnd) {
+        io_->Write(addr, value);
     } else if (addr >= kHramStart && addr <= kHramEnd) {
         hram_[addr - kHramStart] = value;
+    } else if (addr == kInterruptFlags) {
+        io_->Write(addr, value);
+    } else if (addr == kInterruptEnable) {
+        io_->Write(addr, value);
     } else {
         std::cout << "Unimplemented memory write: " << std::hex << addr << "\n";
     }
